a59f9fcc4ea422bb9d831c054f90c723a01debec
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-02 12:21:47 -0500

    Button with Save icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@material-ui/core/Button";
+
+import SaveIcon from "@material-ui/icons/Save";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          size="small"
+          style={{
+            fontSize: 6,
+          }}
+          onClick={() => alert("hello")}
+          variant="contained"
+          color="primary"
+          startIcon={<SaveIcon />}
         >
-          Learn React
-        </a>
+          Save
+        </Button>
       </header>
     </div>
   );
