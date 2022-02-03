f61a508616ee0b7549386746717c9dfb0441ec3a
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-03 08:40:58 -0500

    MUI Themes and ThemeProvider

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ import Checkbox from "@material-ui/core/Checkbox";
 import { useState } from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  ThemeProvider,
+  createTheme,
+} from "@material-ui/core/styles";
+import { green, lightBlue } from "@material-ui/core/colors";
 
+//custom styling for button
 const useStyles = makeStyles({
   root: {
     background: "linear-gradient(45deg, #FE6B8B, #999)",
@@ -24,6 +30,19 @@ function ButtonStyled() {
   const classes = useStyles();
   return <Button className={classes.root}>Test Styled Button</Button>;
 }
+
+//custom theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[500],
+      light: green[200],
+    },
+    secondary: {
+      main: lightBlue[500],
+    },
+  },
+});
 
 function CheckBoxExample() {
   const [checked, setChecked] = useState(true);
@@ -45,35 +64,37 @@ function CheckBoxExample() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ButtonStyled />
-        <TextField
-          variant="filled"
-          color="secondary"
-          type="email"
-          label="Contact me"
-          placeholder="email@email.com"
-        />
-        <CheckBoxExample />
-        <ButtonGroup size="small" variant="contained">
-          <Button
-            color="primary"
-            onClick={() => alert("saved")}
-            startIcon={<SaveIcon />}
-          >
-            Save
-          </Button>
-          <Button
-            color="secondary"
-            onClick={() => alert("discarded")}
-            endIcon={<Delete />}
-          >
-            Discard
-          </Button>
-        </ButtonGroup>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <ButtonStyled />
+          <TextField
+            variant="filled"
+            color="primary.light"
+            type="email"
+            label="Contact me"
+            placeholder="email@email.com"
+          />
+          <CheckBoxExample />
+          <ButtonGroup size="small" variant="contained">
+            <Button
+              color="primary"
+              onClick={() => alert("saved")}
+              startIcon={<SaveIcon />}
+            >
+              Save
+            </Button>
+            <Button
+              color="secondary"
+              onClick={() => alert("discarded")}
+              endIcon={<Delete />}
+            >
+              Discard
+            </Button>
+          </ButtonGroup>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
